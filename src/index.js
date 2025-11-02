041c9956fba010611efcabe4c2c85ea27bf7d69b
PedroPerez <pedroperezsaez.1992@gmail.com>
2025-11-03 00:39:21 +0100

    Haciendo clock

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const clock = document.getElementById('navClock');
+const alarm = document.getElementById('navAlarm');
+const stopwatch = document.getElementById('navStopwatch');
+const timer = document.getElementById('navTimer');
+function clickReloj(){
+    if(clock){
+    clock.addEventListener('click', function(){
+        const seleccionadoantes =document.querySelector('.seleccionado')
+    } );
+}}
