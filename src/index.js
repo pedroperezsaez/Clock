e0cc1c5a8cb839a78326a450648e30692b6afbd6
PedroPerez <pedroperezsaez.1992@gmail.com>
2025-11-08 19:14:08 +0100

    Pagina clock corregida

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-const clock = document.getElementById('navClock');
-const alarm = document.getElementById('navAlarm');
-const stopwatch = document.getElementById('navStopwatch');
-const timer = document.getElementById('navTimer');
-function clickReloj(){
-    if(clock){
-    clock.addEventListener('click', function(){
-        const seleccionadoantes =document.querySelector('.seleccionado')
-    } );
-}}
+const botones= document.querySelectorAll('.botones');
+botones.forEach(boton => {
+    boton.addEventListener('click', ()=>{
+        botones.forEach(botonActual => botonActual.classList.remove('boton-seleccionado'));
+        boton.classList.add('boton-seleccionado');
+    });
+});
