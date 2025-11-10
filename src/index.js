01c2410617b82ecde149124f102f3499c3129e4b
PedroPerez <pedroperezsaez.1992@gmail.com>
2025-11-10 01:04:55 +0100

    iconos alarm cambian de color

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 const botones= document.querySelectorAll('.botones');
-botones.forEach(boton => {
-    boton.addEventListener('click', ()=>{
+const botonStartAlarm=document.querySelector('.start');
+const botonPausaAlarm=document.querySelector('.pause');
+
+
+
+botones.forEach(function(boton) {
+    boton.addEventListener('click', function(){
         botones.forEach(botonActual => botonActual.classList.remove('boton-seleccionado'));
         boton.classList.add('boton-seleccionado');
     });
 });
+
+
+botonStartAlarm.addEventListener('click', function(){
+    botonStartAlarm.classList.add('activo' );
+     botonStartAlarm.classList.remove('inactivo');
+    botonPausaAlarm.classList.add('inactivo');
+    botonPausaAlarm.classList.remove('activo');
+
+});
+
+botonPausaAlarm.addEventListener('click', function(){
+    botonStartAlarm.classList.remove('activo');
+    botonStartAlarm.classList.add('inactivo');
+    botonPausaAlarm.classList.add('activo');
+    botonPausaAlarm.classList.remove('inactivo');
+});
+
